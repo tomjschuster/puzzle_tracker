8c775b77b827de497ae16164bdd83a35943de092
Tom Schuster <tomjschuster@gmail.com>
2022-09-26 22:54:50 -0400

    Initial commit

---
+++ lib/puzzle_tracker.ex
@@ -0,0 +1,18 @@
+defmodule PuzzleTracker do
+  @moduledoc """
+  Documentation for `PuzzleTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PuzzleTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/puzzle_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule PuzzleTrackerTest do
+  use ExUnit.Case
+  doctest PuzzleTracker
+
+  test "greets the world" do
+    assert PuzzleTracker.hello() == :world
+  end
+end
